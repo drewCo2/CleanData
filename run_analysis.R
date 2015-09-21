b3083eca7f15c1b811b58ed1e9b600bb02c7258c
drewCo2 <andrew.a.ritz@gmail.com>
2015-09-21 16:24:03 -0400

    Updated script so that it will load the activity factors.  Not really useful to the project it seems, but worth keeping around.

--- run_analysis.R
+++ run_analysis.R
@@ -1,1 +1,21 @@
-insert code here
+
+
+
+mergeDatasets<-function()
+{
+  DataDir<-"UCI HAR Dataset"
+  
+  # activity labels.
+  alPath<-paste(DataDir,"/activity_labels.txt", sep="")
+  alRaw<-read.fwf(alPath, widths=c(2,18), col.names=c("id", "activity"), header=FALSE)
+  
+  alFactor<-factor(alRaw$id, labels=alRaw$activity)
+
+  alFactor
+  # gee, we could probably find a better way to do this.....
+  #  alData<-data.frame(alRaw)
+    
+  #alData
+  # c(alData)
+}
+
