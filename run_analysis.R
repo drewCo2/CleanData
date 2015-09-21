f951116f9a923cb72b9abcc1a36e729c92d9c61c
drewCo2 <andrew.a.ritz@gmail.com>
2015-09-21 17:16:58 -0400

    Updated script so that it will load the activity factors.  Not really useful to the project it seems, but worth keeping around.

--- run_analysis.R
+++ run_analysis.R
@@ -1,21 +1,53 @@
 
+# convenience function.
+dataDir<-function() { "UCI HAR Dataset" }
 
 
-mergeDatasets<-function()
+
+
+# Loads the feature labels from disk 
+loadFeatures<-function()
 {
-  DataDir<-"UCI HAR Dataset"
+  path<-paste(dataDir(),"/features.txt", sep="")
+  res<-read.table(path)
+  as.vector(res[,2])
+}
+
+# loads a dataset that we will merge later.
+loadDataSet<-function(dataPath, activityPath)
+{
+
+}
+
+
+# merge one or more datasets into a single data set.
+mergeDatasets<-function(dataSets)
+{
+}
+
+
+GetActivityFactors<-function()
+{
+  dataDir<-DataDir()
   
-  # activity labels.
+  # activity labels, as a factor
   alPath<-paste(DataDir,"/activity_labels.txt", sep="")
   alRaw<-read.fwf(alPath, widths=c(2,18), col.names=c("id", "activity"), header=FALSE)
-  
   alFactor<-factor(alRaw$id, labels=alRaw$activity)
-
+  
+  
+  
   alFactor
   # gee, we could probably find a better way to do this.....
   #  alData<-data.frame(alRaw)
-    
+  
   #alData
   # c(alData)
 }
 
+
+# now run the analysis...
+
+testData<- paste(dataDir(), "/test/X_test.txt", sep="")
+testActivity<-paste(dataDir(), "/test/y_test.txt", sep="")
+loadDataSet(testPath, testActivity)
