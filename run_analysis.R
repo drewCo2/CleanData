9d1a2d56b8f2f4bdb536ab65d3338a2b420626c9
drewCo2 <andrew.a.ritz@gmail.com>
2015-09-27 11:15:29 -0400

    More work on data sets.  we are now using activity labels.

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,9 @@
+# clean the workspace + load libs.
+rm(list=ls())
 library(dplyr)
 
+# Data directory relative to your working directory.  Make sure to use setwd() to set
+# an appropriate working directory.
 DataDir<-"UCI HAR Dataset" 
 
 # Loads the feature labels from disk 
@@ -10,21 +14,6 @@ loadFeatures<-function()
   as.vector(res[,2])
 }
 
-# loads a dataset that we will merge later.
-loadDataSet<-function(dataPath, activityPath)
-{
-  cNames<-GetMeasurementNames()
-  data<-read.table(dataPath, col.names = cNames)
-  act<-read.table(activityPath)
-  
-  data
-}
-
-
-# merge one or more datasets into a single data set.
-# mergeDatasets<-function(dataSets)
-# {
-# }
 
 # Get all of the measurement names.
 GetMeasurementNames<-function()
@@ -35,33 +24,58 @@ GetMeasurementNames<-function()
 }
 
 # Get all of the activity names.
-GetActivityFactors<-function()
+GetActivityData<-function()
 {
-  
+  # Note, we could memoize for better performance later.  
   # activity labels, as a factor
   alPath<-paste(DataDir,"/activity_labels.txt", sep="")
-  alRaw<-read.fwf(alPath, widths=c(2,18), col.names=c("id", "activity"), header=FALSE)
-  alFactor<-factor(alRaw$id, labels=alRaw$activity)
+  al<-read.table(alPath, header=FALSE) 
+  al
+}
+
+# loads a dataset that we will merge later.
+loadDataSet<-function(dataPath, activityPath, subjectPath)
+{
+    
+  cNames<-GetMeasurementNames()
+  data<-read.table(dataPath, col.names = cNames)
+
+  # Extract only the standar deviation and mean information.
+  pat<-"mean()|std()"
+  data<-select(data,matches(pat))
+
   
+  # Append subject and activity information to the data set. 
+  al<-GetActivityData()
+  act<-read.table(activityPath)
+  data$Activity<-factor(act$V1, levels=al[,1], labels=al[,2])
+
+#  subject<-read.table(subjectPath)
+#  data$Subject<-subject
   
-  
-  alFactor
-  # gee, we could probably find a better way to do this.....
-  #  alData<-data.frame(alRaw)
-  
-  #alData 
-  # c(alData)
+  data
 }
 
 
-# now run the analysis...
 
+# Load the test data.  'loadDataSet' function extracts mean/std information + sets subject/activity data.
 testData<- paste(DataDir, "/test/X_test.txt", sep="")
 testActivity<-paste(DataDir, "/test/y_test.txt", sep="")
-testSet<-loadDataSet(testData, testActivity)
+testSubject<-paste(DataDir, "/test/subject_test.txt", sep="")
+testSet<-loadDataSet(testData, testActivity, testSubject)
 
+# Load the train data.
 trainData<- paste(DataDir, "/train/X_train.txt", sep="")
 trainActivity<-paste(DataDir, "/train/y_train.txt", sep="")
-trainSet<-loadDataSet(trainData, trainActivity)
+trainSubject<-paste(DataDir, "/train/subject_train.txt", sep="")
+trainSet<-loadDataSet(trainData, trainActivity, trainSubject)
 
-rt<-read.table(testData)
+#merge the data.
+mSet<-rbind(testSet, trainSet)
+
+# Determine which columns we want to extract from.  Use a pattern match.
+#cIndexes<-grep(pat,colnames(mSet))
+
+#final<-mSet[cIndexes,]
+
+
